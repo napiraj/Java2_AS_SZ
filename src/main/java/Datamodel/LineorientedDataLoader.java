b5efcb1e3a56244c8d1b1eb46f88ddcb2ac77ec6
napiraj <sabrina_zgraggen@hotmail.com>
2015-05-18 12:12:49 +0200

    Programm vom 13.5 in Projektordner auf Git Hub kopiert

---
+++ src/main/java/Datamodel/LineorientedDataLoader.java
@@ -0,0 +1,15 @@
+package Datamodel;
+
+import java.io.File;
+import java.util.ArrayList;
+/**
+ * Created by Alexandra on 12.05.2015.
+ */
+public class LineorientedDataLoader implements DataLoader{
+    public DataModel loadDataModel(File filename){
+
+        return null;
+    }
+
+
+}
