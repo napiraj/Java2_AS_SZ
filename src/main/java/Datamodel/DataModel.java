bce2db87a95313acac55f4fd7732548a35ef413c
napiraj <sabrina_zgraggen@hotmail.com>
2015-05-20 12:11:17 +0200

    Programm vom 13.5 in Projektordner auf Git Hub kopiert

--- src/main/java/Datamodel/DataModel.java
+++ src/main/java/Datamodel/DataModel.java
@@ -6,12 +6,15 @@ import java.util.ArrayList;
  */
 public class DataModel {
     private String name;
+    private String name2;
+
     private ArrayList<Double> valueVariable1;
     private ArrayList<Double> valueVariable2;
 
 
-    public DataModel(String name,ArrayList<Double> valueVariable1, ArrayList<Double> valueVariable2){
+    public DataModel(String name, String name2, ArrayList<Double> valueVariable1, ArrayList<Double> valueVariable2){
         this.name=name;
+        this.name2=name2;
         this.valueVariable1 = valueVariable1;
         this.valueVariable2=valueVariable2;
 
@@ -19,11 +22,18 @@ public class DataModel {
 
     }
 
-    public String getName(){
+    public String getName1(){
         return name;
     }
-    public ArrayList getWerte(){
+    public String getName2(){
+        return name2;
+    }
+
+    public ArrayList getValue1(){
         return valueVariable1;
+    }
+    public ArrayList getValue2(){
+        return valueVariable2;
     }
     public double getMin(){
         return 0;
