7279c3061d9919c64987c60b4d5b803607a59b42
napiraj <sabrina_zgraggen@hotmail.com>
2015-05-20 12:26:57 +0200

    Programm vom 13.5 in Projektordner auf Git Hub kopiert

--- src/main/java/Datamodel/LineorientedDataLoader.java
+++ src/main/java/Datamodel/LineorientedDataLoader.java
@@ -1,12 +1,24 @@
 package datamodel;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  * Created by Alexandra on 12.05.2015.
  */
 public class LineorientedDataLoader implements DataLoader{
-    public DataModel loadDataModel(File filename){
+    public DataModel loadDataModel(File file){
+
+        Scanner inputScanner=null;
+
+        try {
+            inputScanner= new Scanner(file);
+        } catch (FileNotFoundException e) {
+            return null;
+        }
+
+
 
         return null;
     }
