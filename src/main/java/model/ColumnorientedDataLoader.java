6f651919ca47db147eb7503adc486fbda5c115f7
napiraj <sabrina_zgraggen@hotmail.com>
2015-05-27 11:21:59 +0200

    27.05.15

--- src/main/java/model/ColumnorientedDataLoader.java
+++ src/main/java/model/ColumnorientedDataLoader.java
@@ -17,32 +17,49 @@ public class ColumnorientedDataLoader implements DataLoader{
         try {
             inputScanner= new Scanner(file);
         } catch (FileNotFoundException e) {
-            return null;
+            System.err.println("File not found.");
         }
-        double valueParse1;
-        double valueParse2;
+
         String variableName1=" ";
+        String variableName2=" ";
+        ArrayList<String> allVariables = new ArrayList<String>();
         ArrayList<Double> valuesList1=new ArrayList<Double>();
         ArrayList<Double> valuesList2= new ArrayList<Double>();
 
 
         String line = inputScanner.nextLine();
+        double countVariables = line.split("\t").length;
+
         variableName1 = line.split("\t")[0];
-        String variableName2 = line.split("\t")[1];
+        variableName2 = line.split("\t")[1];
+        allVariables.add(variableName1);
+        allVariables.add(variableName2);
+
         while(inputScanner.hasNextLine()) {
             String valueRead1 = inputScanner.next();
             String valueRead2= inputScanner.next();
-            valueParse1 = Double.parseDouble(valueRead1);
-            valueParse2= Double.parseDouble(valueRead2);
-            valuesList1.add(valueParse1);
-            valuesList2.add(valueParse2);
 
-            Variable variable1 = new Variable(variableName1, variableName2, valuesList1, valuesList2);
-            //Variable variable2 = new Variable();
+            valuesList1.add(Double.parseDouble(valueRead1));
+            valuesList2.add(Double.parseDouble(valueRead2));
 
         }
+        ArrayList<Double> valuesOfVariable = new ArrayList<Double>();
 
-        return new Variable(variableName1, variableName2, valuesList1, valuesList2);
+        for (int i = 0; i < valuesList1.size(); i++){
+        valuesOfVariable.add(valuesList1.get(i));}
+
+        for (int i = 0; i < valuesList2.size(); i++){
+            valuesOfVariable.add(valuesList2.get(i));}
+
+
+       /* Variable var1 = new DataModel(allVariables, valuesList1);
+        Variable var2 = new Variable(variableName2, valuesList1);
+
+        ArrayList<Variable> varlist = new ArrayList<Variable>();
+        varlist.add(var1);
+        varlist.add(var2); */
+
+        return new DataModel(allVariables, countVariables, valuesOfVariable);
     }
 
 }
