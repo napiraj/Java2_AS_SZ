60a12564598fd8d62a45727a815ddbce364c4a80
lexichan <s.ardnaxela@web.de>
2015-05-27 12:13:05 +0200

    GUI angefangen

--- src/main/java/model/Variable.java
+++ src/main/java/model/Variable.java
@@ -6,19 +6,61 @@ import java.util.ArrayList;
  * Created by Alexandra on 13.05.2015.
  */
 public class Variable  extends DataModel{
-    private String nameOfTheClassVariable;
-    private String nameOfTheClassVariable2;
-    private ArrayList<Double> valuesOfTheClass1;
-    private ArrayList<Double> valuesOfTheClass2;
+    private String name;
 
+    private ArrayList<Double> values;
 
-    public Variable(String name, String name2, ArrayList<Double> valueVariable1, ArrayList<Double> valueVariable2) {
-        super(name, name2, valueVariable1, valueVariable2);
-        nameOfTheClassVariable=name;
-        nameOfTheClassVariable2=name2;
-        valuesOfTheClass1 = valueVariable1;
-        valuesOfTheClass2= valueVariable2;
+    public Variable(String name, ArrayList<Double> valueVariable) {
+        super(name, values);
+        name=name;
+
+        values= valueVariable;
     }
+
+    public String getName(){
+        return name;
+    }
+
+    public ArrayList getValue(){
+        return values;
+    }
+
+    public int getNumberOfValue1(){
+        return values.size();
+    }
+
+
+    public double getMin() {
+        double smallestValueVariable1 = values.get(0);
+
+        for (int i = 1; i < values.size(); i++) {
+            if (values.get(i) < smallestValueVariable1) {
+                smallestValueVariable1 = values.get(i);
+
+            }
+
+
+        }
+        return smallestValueVariable1;
+    }
+
+
+
+    public double getMax(){
+        double largestValueVariable1= values.get(0);
+
+        for (int i= 1; i<values.size();i++) {
+            if (values.get(i) > largestValueVariable1) {
+                largestValueVariable1 = values.get(i);
+
+            }
+        }
+
+        return largestValueVariable1;
+    }
+
+
+
 
 
 }
