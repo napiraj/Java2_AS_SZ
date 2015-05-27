fee9c730aa8b30751c82fbb46bb9194157fef896
napiraj <sabrina_zgraggen@hotmail.com>
2015-05-27 11:22:41 +0200

    27.05.15

--- src/main/java/model/Variable.java
+++ src/main/java/model/Variable.java
@@ -5,20 +5,17 @@ import java.util.ArrayList;
 /**
  * Created by Alexandra on 13.05.2015.
  */
-public class Variable  extends DataModel{
-    private String nameOfTheClassVariable;
-    private String nameOfTheClassVariable2;
-    private ArrayList<Double> valuesOfTheClass1;
-    private ArrayList<Double> valuesOfTheClass2;
+public class Variable {
+    private String nameOfVariable;
+    private ArrayList<Double> valuesOfVariable;
 
 
-    public Variable(String name, String name2, ArrayList<Double> valueVariable1, ArrayList<Double> valueVariable2) {
-        super(name, name2, valueVariable1, valueVariable2);
-        nameOfTheClassVariable=name;
-        nameOfTheClassVariable2=name2;
-        valuesOfTheClass1 = valueVariable1;
-        valuesOfTheClass2= valueVariable2;
+
+    public Variable(String nameOfVariable, ArrayList<Double> values) {
+        this.nameOfVariable = nameOfVariable;
+        this.valuesOfVariable = values;
     }
+
 
 
 }
