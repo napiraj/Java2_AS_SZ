fa9855ba1d4ee8acc145ae445556902252bc3367
napiraj <sabrina_zgraggen@hotmail.com>
2015-05-27 11:22:33 +0200

    27.05.15

--- src/main/java/gui/MainPanel.java
+++ src/main/java/gui/MainPanel.java
@@ -3,6 +3,7 @@ package gui;
 import model.DataLoader;
 import model.DataModel;
 import model.Variable;
+import model.*;
 
 import javax.swing.*;
 import javax.xml.crypto.Data;
@@ -16,11 +17,24 @@ import java.util.List;
  * Created by Alexandra on 20.05.2015.
  */
 public class MainPanel extends JPanel {
+
+    static ArrayList<String> name;
+    static double countVariable;
+    static ArrayList<Double> valueVariable;
+
     public MainPanel() {
-        ArrayList<Double> values= new ArrayList<Double>();
-        values.add(DataModel.getValue1());
-        Histogram drawingPanel = new Histogram(values);
+
+        DataModel model1 = new DataModel(name, countVariable, valueVariable);
+
+        JPanel panel = new JPanel();
+
+
+        ArrayList<Double> valuesX= new ArrayList<Double>(valueVariable);
+
+        Histogram drawingVariable1 = new Histogram(valuesX);
+      //  Histogram drawingVariable2 = new Histogram(valuesY);
         setLayout(new BorderLayout());
-        add(drawingPanel);
+        panel.add(drawingVariable1);
+      //  panel.add(drawingVariable2);
     }
 }
